0c72f9af8961f7d02c370ab7386dce145b07802a
marialazv <holamarialazaro@gmail.com>
2023-10-26 15:13:23 +0200

    Responsive

--- consola.js
+++ consola.js
@@ -1,8 +1,13 @@
 function cambiarImagen(accion) {
-    var img = document.getElementById("imgchange")
+    var img = document.getElementById("imgchange");
+    
     if (accion === 'siguiente') {
         img.src = 'css/img/videojuego2.jpeg';
+        contador = 1
     } else if (accion === 'reset') {
         img.src= 'css/img/pantallainicio.png';
+    } else if (accion === 'siguiente'){
+        img.src = 'css/img/videojuego1.avif';
+
     }
 }
