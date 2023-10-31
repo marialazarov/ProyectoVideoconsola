0c0359b827014f42c77d617cc3c38af9456e169e
marialazv <holamarialazaro@gmail.com>
2023-10-31 18:51:41 +0100

    Ultimos cambios

--- consola.js
+++ consola.js
@@ -1,13 +1,34 @@
-function cambiarImagen(accion) {
-    var img = document.getElementById("imgchange");
-    
-    if (accion === 'siguiente') {
-        img.src = 'css/img/videojuego2.jpeg';
-        contador = 1
-    } else if (accion === 'reset') {
-        img.src= 'css/img/pantallainicio.png';
-    } else if (accion === 'siguiente'){
-        img.src = 'css/img/videojuego1.avif';
+let imagenes = ['css/img/juego1.gif', 'css/img/juego2.webp', 'css/img/juego3.webp'];
+let contador = 0;
+let encendida = true;
 
+function Encenderoapagar() {
+    encendida = !encendida;
+    const img = document.getElementById('imgchange');
+    const botonReset = document.getElementById('botonReset');
+    
+    if (encendida) {
+        img.src = 'css/img/inicio.webp';
+        botonReset.disabled = false; 
+    } else {
+        img.src = 'css/img/apagado.png';
+        botonReset.disabled = true; 
     }
 }
+
+function Reset() {
+    if (encendida) {
+        const img = document.getElementById('imgchange');
+        img.src = 'css/img/inicio.webp';
+    }
+}
+
+function cambiarImagen() {
+    if (encendida) {
+        contador = (contador + 1) % imagenes.length;
+        const img = document.getElementById('imgchange');
+        img.src = imagenes[contador];
+    }
+}
+
+
